365aa1d25a57300f5ad283a53d0993af793af1a5
hyungjin kim <hjux07@gmail.com>
2023-03-14 12:00:28 +0900

    style: menu, header 완성

--- final-cra/src/components/Header.js
+++ final-cra/src/components/Header.js
@@ -1,97 +1,201 @@
-import styled from 'styled-components';
-import headerAni from './headerAni.css';
-// import hteme from './../../public/style/theme.css';
+import styled, { keyframes } from 'styled-components';
 import HeaderWave from './HeaderWave.js';
-import React, { useState, useEffect, useRef } from 'react'
+import { pink } from './../style/theme';
+import { ReactComponent as Cart } from './../assets/icons/btn-cart.svg';
+import { ReactComponent as User } from './../assets/icons/btn-user.svg';
+import { useState, useEffect, useRef } from 'react';
 
-
+const design = keyframes`
+	0% {transform:translateY(20px) rotate(45deg);}
+	50% {transform:translateY(20px) rotate(0);}
+	100% {transform:translateY(0) rotate(0);}
+`;
+const design2 = keyframes` 
+  0% {transform:translateY(-20px) rotate(-45deg);}
+	50% {transform:translateY(-20px) rotate(0);}
+	100% {transform:translateY(0) rotate(0);}
+`;
+const activeDesign = keyframes`
+	0% {transform:translateY(0) rotate(0);}
+	50% {transform:translateY(20px) rotate(0);}
+	100% {transform:translateY(20px) rotate(45deg);}
+`;
+const activeDesign2 = keyframes`
+	0% {transform:translateY(0) rotate(0);}
+	50% {transform:translateY(-20px) rotate(0);}
+	100% {transform:translateY(-20px) rotate(-45deg);}
+`;
 const HeaderWapper = styled.header`
   width: 100%;
-  height: 100px;
-  background-color: var(--pink);
+  height: 6.25rem;
+  background-color: transparent;
   display: flex;
   justify-content: space-between;
   align-items: center;
   padding: 0 2.5rem;
+  position: fixed;
+  z-index: 3;
+  transform: all 0.6s;
 
-  .menuBtn{
-    width: 35px;
-    height: 16px;
+  ul.menuBtn {
+    width: 2.1875rem;
+    height: 1rem;
+    position: relative;
+    cursor: pointer;
 
-    ul{
-      position: relative;
+    > li {
+      position: absolute;
+      left: 0;
       width: 100%;
       height: 100%;
-      cursor:pointer;
+      height: 0.125rem;
+      background-color: #fff;
+      border-radius: 0.0625rem;
 
-      li{
-        position: absolute;
-          left: 0;
-          width: 100%;
-          height: 100%;
-          height: 2px;
-          background-color: #fff;
-          border-radius: 1px;
-
-        &:nth-of-type(1){
-          top:0;
-          animation: design .75s forwards;
-        }
-
-        &:nth-of-type(2){
-          top: 7px;
-          transition: all .25s .25s;
-          opacity: 1;
-        }
-
-        &:nth-of-type(3){
-          bottom: 0;
-          animation: design2 .75s forwards;
-        }
-
+      &:nth-of-type(1) {
+        top: 0;
+        animation: ${design} 0.75s forwards;
       }
-    }
 
-    ul.active{
-      li{
-        &:nth-of-type(1){
-          top:-12px;
-          animation: active-design .75s forwards;
-        }
-        &:nth-of-type(2){
-              opacity: 0;
-          animation: active-design .75s forwards;
-        }
-        &:nth-of-type(3){
-              bottom: -12px;
-              animation: active-design2 .75s forwards;
-        }
+      &:nth-of-type(2) {
+        top: 0.4375rem;
+        transition: all 0.25s 0.25s;
+        opacity: 1;
+      }
 
+      &:nth-of-type(3) {
+        bottom: 0;
+        animation: ${design2} 0.75s forwards;
       }
     }
   }
-`
 
+  .menuBtn.active {
+    li {
+      &:nth-of-type(1) {
+        top: -0.75rem;
+        animation: ${activeDesign} 0.75s forwards;
+      }
+      &:nth-of-type(2) {
+        opacity: 0;
+        animation: ${activeDesign} 0.75s forwards;
+      }
+      &:nth-of-type(3) {
+        bottom: -0.75rem;
+        animation: ${activeDesign2} 0.75s forwards;
+      }
+    }
+  }
+
+  h1.logo {
+    width: 18.75rem;
+    height: 2.25rem;
+    cursor: pointer;
+    margin-left: 2.625rem;
+
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+  }
+
+  .infoWrap {
+    display: flex;
+    gap: 1.25rem;
+  }
+`;
+const SubHeader = styled.div`
+  position: absolute;
+  left: 0;
+  top: 6.25rem;
+  width: 100%;
+  height: 3.875rem;
+  background: transparent;
+  border-top: 1px solid rgba(255, 255, 255, 0.5);
+  border-bottom: 1px solid rgba(255, 255, 255, 0.5);
+  transition: all 1s;
+  z-index: 2;
+  display: flex;
+  justify-content: center;
+
+  ul {
+    height: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 1108px;
+
+    li {
+      font: 600 1rem/1 'arial';
+      cursor: pointer;
+      color: #fff;
+      position: relative;
+
+      &::after {
+        content: '';
+        display: block;
+        position: absolute;
+        top: 50%;
+        left: -70px;
+        width: 8px;
+        height: 8px;
+        border-radius: 50%;
+        background-color: rgba(255, 255, 255, 0.45);
+        transform: translateY(-50%);
+      }
+
+      &:first-child::after {
+        display: none;
+      }
+    }
+  }
+`;
 
 const Header = () => {
-
   const [menu, menuSetState] = useState(false);
 
   return (
-
     <>
-    <HeaderWave menu={menu}/>
-    <HeaderWapper>
+      <HeaderWave menu={menu} />
 
-      <div className="menuBtn" onClick={()=>menuSetState(!menu)}>
-        <ul className={!menu? 'active':''}>
-          {Array(3).fill().map((el,idx)=><li key={idx}>{el}</li>)}
+      <HeaderWapper>
+        <ul
+          className={!menu ? 'menuBtn' : 'menuBtn active'}
+          onClick={() => menuSetState(!menu)}
+        >
+          {Array(3)
+            .fill()
+            .map((el, idx) => (
+              <li key={idx}>{el}</li>
+            ))}
         </ul>
-      </div>
 
-    </HeaderWapper>
+        <h1 className="logo">
+          <img
+            src={require('../../src/assets/images/logo-on.png')}
+            alt="logoOn"
+          />
+        </h1>
+
+        <div className="infoWrap">
+          <User style={{ width: 28, height: 30, fill: '#fff' }} />
+          <Cart style={{ width: 28, height: 30, fill: '#fff' }} />
+        </div>
+      </HeaderWapper>
+
+      <SubHeader>
+        <ul>
+          <li>RESERVATION</li>
+          <li>PIC</li>
+          <li>GUAM</li>
+          <li>SAIPAN</li>
+          <li>NOTICE</li>
+          <li>COMMUNITY</li>
+        </ul>
+      </SubHeader>
     </>
-  )
-}
+  );
+};
 
 export default Header;
--- final-cra/src/components/Visual.js
+++ final-cra/src/components/Visual.js
@@ -1,9 +1,48 @@
-import React from 'react'
+import React,{useEffect, useRef} from 'react';
+import styled from 'styled-components';
+import gsap from 'gsap';
+
+const Titles = styled.ul`
+
+li{
+
+  span{
+
+  }
+}
+`;
+
+const VisualWapper = styled.figure`
+  width: 100%;
+  height: 100vh;
+  overflow: hidden;
+  background-color: #222;
+
+  video {
+    width: 100%;
+    height: 100%;
+    opacity: 0.8;
+    object-fit: cover;
+  }
+`;
 
 const Visual = () => {
   return (
-    <div>Visual</div>
-  )
-}
+    <VisualWapper>
+      <Titles>
+        {
+          
+        }
+      </Titles>
 
-export default Visual
+      <video
+        autoPlay
+        loop
+        muted
+        src={require('../../src/assets/images/visual-video.mp4')}
+      ></video>
+    </VisualWapper>
+  );
+};
+
+export default Visual;
