aec603334f3ac7f1a1f8c7a44f70385d4e51171e
hyungjin kim <hjux07@gmail.com>
2023-03-14 14:42:28 +0900

    feat: visual 타이틀 인터랙션 구현

--- final-cra/src/components/Visual.js
+++ final-cra/src/components/Visual.js
@@ -1,15 +1,40 @@
-import React,{useEffect, useRef} from 'react';
+import React, { useEffect, useRef } from 'react';
 import styled from 'styled-components';
 import gsap from 'gsap';
 
 const Titles = styled.ul`
+  width: 100%;
+  height: 100%;
+  position: absolute;
 
-li{
+  > li {
+    width: 100%;
+    text-align: center;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 2;
+    display: flex;
+    justify-content: center;
 
-  span{
+    > span {
+      display: inline-block;
+      font: 600 8.75rem/1 'arial';
+      text-transform: uppercase;
+      color: #fff;
 
+      &:nth-last-of-type(1),
+      &:nth-last-of-type(2),
+      &:nth-last-of-type(3) {
+        -webkit-text-stroke: 2px #fff;
+        color: transparent;
+      }
+      &:nth-last-of-type(3) {
+        margin-left: 1.75rem;
+      }
+    }
   }
-}
 `;
 
 const VisualWapper = styled.figure`
@@ -26,13 +51,45 @@ const VisualWapper = styled.figure`
   }
 `;
 
+const titles = [
+  'Exciting PIC',
+  'Kid Friendly PIC',
+  'Romantic PIC',
+  'Relaxing PIC',
+];
+
 const Visual = () => {
+  useEffect(() => {
+    const tl = gsap.timeline({
+      repeat: -1,
+      defaults: {
+        duration: 1,
+        y: 100,
+        opacity: 0,
+        ease: 'power3.inOut',
+        stagger: { each: 0.2 },
+        onComplete() {
+          gsap.to(this.targets(), { opacity: 0 });
+        },
+      },
+    });
+    tl.from('.title1 > span', {});
+    tl.from('.title2 > span', {});
+    tl.from('.title3 > span', {});
+    tl.from('.title4 > span', {});
+  }, []);
+
   return (
     <VisualWapper>
       <Titles>
-        {
-          
-        }
+        {titles.map((el, idx) => (
+          <li className={`title${idx + 1}`} key={el}>
+            {[...el.split(' ')[0]].map((el) => (<span key={el}>{el}</span>))}
+            <span>P</span>
+            <span>I</span>
+            <span>C</span>
+          </li>
+        ))}
       </Titles>
 
       <video
