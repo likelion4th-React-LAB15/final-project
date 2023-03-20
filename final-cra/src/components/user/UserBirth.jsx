ec42c8b780d82f727b3779d99c7a9fc9341a2a77
hungerbk <wjantlr@gmail.com>
2023-03-21 00:05:42 +0900

    feat: UserBirth 컴포넌트를 UserBirthInput 컴포넌트로 변경
    - forwardRef를 이용하여 ref 참조를 위해 컴포넌트 수정
    - UserInput 컴포넌트를 forwardRef를 사용하면서 UserBirth 인풋에 스타일 미적용 문제 해결
    
    #14
    
    Co-authored-by: devDobby <porphyria95@gmail.com>

--- final-cra/src/components/user/UserBirth.jsx
+++ final-cra/src/components/user/UserBirth.jsx
@@ -1,33 +1,25 @@
-import { UserSignUpInput } from "components/user"
 import styled from "styled-components"
-import theme from 'style/theme'
+import { useState, useId, forwardRef } from "react"
 
-export const UserBirth = ({...rest}) => {
-  return(
-    <StyledBirth>
-      <StyledBirthInput name={'birthYear'} labelClass={'a11y-hidden'} placeholder={'YYYY'} maxLength="4" number {...rest}>연도 입력(4글자)</StyledBirthInput> 
-      <StyledBirthInput name={'birthMonth'} labelClass={'a11y-hidden'} placeholder={'MM'} maxLength="2" number {...rest}>월 입력(2글자)</StyledBirthInput>
-      <StyledBirthInput name={'birthDay'} labelClass={'a11y-hidden'} placeholder={'DD'} maxLength="2" number {...rest}>일 입력(2글자)</StyledBirthInput>
-    </StyledBirth>
+export const UserBirthInput = forwardRef((props,ref) => {
+  const [ isNumber, setIsNumber] = useState('')
+  const UserInputId = useId();
+
+  const handleNumber = (e) =>{
+    const result = e.target.value.replace(/\D/g, '');
+    setIsNumber(result);
+    }
+
+  return (
+    <>
+      <label className="a11y-hidden" htmlFor={UserInputId}></label>
+      <StyledBirthInput name={props.name} placeholder={props.placeholder} maxLength={props.maxLength} id={UserInputId} ref={ref} onInput={handleNumber} value={isNumber} onChange={props.onChange}></StyledBirthInput>
+    </>
   )
-}
 
-const StyledBirth = styled.div` 
-    width: 21.25rem;
-    height: 3.125rem;
-    margin: ${theme.spacingXs} 0;
-    border: 0.0625rem solid #A6A6A6;
-    border-radius: 3.125rem;
-    background-color: ${theme.white};
-    display: flex;
-    flex-flow: row nowrap;
-    justify-content: center;
-    align-items: center;
-    position: relative;
-    left: 8.75rem;
-    top: -2.5rem;
-    `
-const StyledBirthInput = styled(UserSignUpInput)`
+})
+
+const StyledBirthInput = styled.input`
   width: 4rem;
   height: 2rem;
   text-indent: 0;
