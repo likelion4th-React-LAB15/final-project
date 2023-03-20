609fe439a69149dd32cb79645504df0f066d14bf
Yujin Lee <jenny7732@naver.com>
2023-03-20 09:12:31 +0900

    feat: px 단위 rem 단위로 변경

--- final-cra/src/pages/Reservation1.jsx
+++ final-cra/src/pages/Reservation1.jsx
@@ -3,6 +3,7 @@ import StepNavigation from "components/StepNavigation.js"
 import GuestSelect from "components/GuestSelect.js"
 import ContinueButton from "components/ContinueButton.js"
 import ReservationTitle from "components/ReservationTitle.js";
+import theme from 'style/theme';
 
 const GuestNumber = styled.h3`
   font-family: 'Inter';
@@ -11,7 +12,7 @@ const GuestNumber = styled.h3`
   font-size: 2rem;
   line-height: 2.375rem;
   text-align: center;
-  color: ${(props) => props.theme.blue};
+  color: ${theme.blue};
   margin-top: 5.688rem;
 `
 
@@ -19,17 +20,18 @@ const ButtonWrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  margin-bottom: 24.063rem;
 `
 
 export const Reservation1 = () => {
 
   return (
     <ReservationTitle value={"투숙객"}>
-    <StepNavigation labelArray={['투숙객 선택', '날짜 선택', '객실 선택']}></StepNavigation>
+    <StepNavigation page = {'reservation1'} labelArray={['투숙객 선택', '날짜 선택', '객실 선택']}></StepNavigation>
     <GuestNumber>투숙객 및 객실</GuestNumber>
     <GuestSelect></GuestSelect>
     <ButtonWrapper>
-      <ContinueButton value={'확인 및 계속'}></ContinueButton>
+      <ContinueButton id={'reservation2'} value={'확인 및 계속'}></ContinueButton>
     </ButtonWrapper>
     </ReservationTitle>
   )
--- final-cra/src/pages/Reservation2.jsx
+++ final-cra/src/pages/Reservation2.jsx
@@ -1,6 +1,6 @@
 import ReservationTitle from "components/ReservationTitle.js";
 import StepNavigation from "components/StepNavigation.js"
-import ReservationCalender from "components/ReservationCalender.js"
+import ReservationCalendar from "components/ReservationCalendar.js"
 import ContinueButton from "components/ContinueButton.js";
 import BackButton from "components/BackButton.js";
 import styled from 'styled-components';
@@ -9,17 +9,18 @@ const ButtonWrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  gap:37px;
+  gap: 2.313rem;
+  margin-bottom: 15.168rem;
 `
 
 export const Reservation2 = () => {
 
   return (
  <ReservationTitle value={"날짜"}>
-  <StepNavigation labelArray={['투숙객 선택', '날짜 선택', '객실 선택']}></StepNavigation>
-  <ReservationCalender/>
+  <StepNavigation page={'reservation2'} labelArray={['투숙객 선택', '날짜 선택', '객실 선택']}></StepNavigation>
+  <ReservationCalendar/>
     <ButtonWrapper>
-      <BackButton value={'뒤로'}/>
+      <BackButton id={'reservation1'} value={'뒤로'}/>
       <ContinueButton value={'숙박 기간 및 확인'}/>
     </ButtonWrapper>
  </ReservationTitle>
