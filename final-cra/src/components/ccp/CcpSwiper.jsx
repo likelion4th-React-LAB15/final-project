e73cfa532b54f50e4540a1505b4759d8db004e1f
Jihoon <jangna96@gmail.com>
2023-03-22 21:42:22 +0900

    fix: 스와이퍼 버그 픽스
    - 마우스 hover시 delay 무시하고 슬라이드 되는 버그 픽스
    - 마지막 슬라이드에서 다음 슬라이드가 보이지않는 버그 픽스
    - 접근성을 고려하여 컨트롤러 추가
    - autoplay를 제어할 수 있는 start&stop 버튼 추가

--- final-cra/src/components/ccp/CcpSwiper.jsx
+++ final-cra/src/components/ccp/CcpSwiper.jsx
@@ -1,25 +1,45 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
-import SwiperCore, { Pagination, Autoplay, Keyboard, A11y } from 'swiper';
+import SwiperCore, {
+  Navigation,
+  Pagination,
+  Autoplay,
+  Keyboard,
+  A11y,
+} from 'swiper';
 import styled from 'styled-components/macro';
 import theme from 'style/theme';
 import 'swiper/swiper-bundle.min.css';
 import 'swiper/swiper.min.css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
+import 'swiper/css/controller';
+import { useRef, useState } from 'react';
 
-SwiperCore.use([Pagination, Autoplay, Keyboard]);
+import playIcon from 'assets/icons/icon-swiper-play.svg';
+import stopIcon from 'assets/icons/icon-swiper-stop.svg';
+
+SwiperCore.use([Pagination, Autoplay, Keyboard, A11y]);
+
+const swipeData = [
+  { id: 1, src: 'golf-img1.jpg', alt: '골프장 배경1' },
+  { id: 2, src: 'golf-img2.jpg', alt: '골프장 배경2' },
+  { id: 3, src: 'golf-img3.jpg', alt: '골프장 배경3' },
+  { id: 4, src: 'golf-img4.jpg', alt: '골프장 배경4' },
+];
 
 export const CcpSwiper = ({ children }) => {
+  const swiperRef = useRef(null);
+  const [swiperControl, setSwiperControl] = useState(true);
   return (
     <StyledCcpSwiper>
       <Swiper
+        ref={swiperRef}
         spaceBetween={100}
         slidesPerView={'auto'}
         centeredSlides={true}
+        navigation={true}
         autoplay={{
           delay: 7000,
-          disableOnInteraction: false,
-          pauseOnMouseEnter: true,
         }}
         speed={1500}
         pagination={{
@@ -30,48 +50,75 @@ export const CcpSwiper = ({ children }) => {
           nextSlideMessage: '다음 슬라이드',
         }}
         loop={true}
+        // 컨트롤러, 키보드 접근
         keyboard
-        modules={[Autoplay, Pagination, A11y]}
+        modules={[Navigation, Autoplay, Pagination, A11y]}
         className="activeSlideWrap"
       >
-        <SwiperSlide>
-          <img
-            src={require('assets/images/golf-img1.jpg')}
-            alt="CCP 괌 골프장 배경1"
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img
-            src={require('assets/images/golf-img2.jpg')}
-            alt="CCP 괌 골프장 배경2"
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img
-            src={require('assets/images/golf-img3.jpg')}
-            alt="CCP 괌 골프장 배경3"
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img
-            src={require('assets/images/golf-img2.jpg')}
-            alt="CCP 괌 골프장 배경2"
-          />
-        </SwiperSlide>
+        {swipeData.map((data) => {
+          return (
+            <SwiperSlide
+              key={data.id}
+              onMouseEnter={() => swiperRef.current.swiper.autoplay.stop()}
+              onMouseLeave={() => swiperRef.current.swiper.autoplay.start()}
+            >
+              <img src={require(`assets/images/${data.src}`)} alt={data.alt} />
+            </SwiperSlide>
+          );
+        })}
       </Swiper>
+      <div className="buttonWrap">
+        <button
+          className={swiperControl ? 'off' : ''}
+          onClick={() => {
+            swiperRef.current.swiper.autoplay.start();
+            setSwiperControl((swiperControl) => (swiperControl = true));
+          }}
+        >
+          <img src={playIcon} alt="시작" />
+        </button>
+        <button
+          className={!swiperControl ? 'off' : ''}
+          onClick={() => {
+            swiperRef.current.swiper.autoplay.stop();
+            setSwiperControl((swiperControl) => (swiperControl = false));
+          }}
+        >
+          <img src={stopIcon} alt="정지" />
+        </button>
+      </div>
     </StyledCcpSwiper>
   );
 };
 
 const StyledCcpSwiper = styled.div`
   width: 100vw;
-  /* left: 50%;
-  transform: translateX(-50%); */
-  /* min-width: 1400px; */
   position: relative;
-  padding-bottom: 85px;
+  padding-bottom: 5.3125rem;
+  .buttonWrap {
+    margin: 2.5rem auto 0 auto;
+    text-align: right;
+    max-width: 100rem;
+    width: 80%;
+  }
+  button {
+    width: 2.5rem;
+    height: 2.5rem;
+    padding: 0;
+    border: 0;
+    margin-left: 0.625rem;
+    cursor: pointer;
+    background-color: ${theme.indigo};
+    border-radius: 50%;
+    color: ${theme.white};
+    pointer-events: visible;
+  }
+  button.off {
+    background-color: #c9c9c9;
+    pointer-events: none;
+  }
   .activeSlideWrap {
-    max-width: 1600px;
+    max-width: 100rem;
     overflow: visible;
     width: 80%;
   }
@@ -86,8 +133,7 @@ const StyledCcpSwiper = styled.div`
     align-items: center;
     padding-bottom: 54.3%;
     border-radius: 1.875rem;
-    background: #f00;
-    font-size: 18px;
+    font-size: 1.125rem;
     text-align: center;
     opacity: 0.2;
     transition: opacity 0.5s ease;
@@ -105,11 +151,11 @@ const StyledCcpSwiper = styled.div`
     opacity: 1;
   }
   .swiper-pagination {
-    bottom: -40px;
+    bottom: -2.5rem;
   }
   .swiper-pagination-bullet {
-    width: 14px;
-    height: 14px;
+    width: 0.875rem;
+    height: 0.875rem;
     background-color: ${theme.gray};
   }
   .swiper-pagination-bullet-active {
@@ -118,6 +164,31 @@ const StyledCcpSwiper = styled.div`
   .swiper-horizontal > .swiper-pagination-bullets .swiper-pagination-bullet,
   .swiper-pagination-horizontal.swiper-pagination-bullets
     .swiper-pagination-bullet {
-    margin: 0 10px;
+    margin: 0 0.625rem;
+  }
+  .swiper-button-next,
+  .swiper-button-prev {
+    transform: translateY(-30%);
+    background-color: rgba(0, 0, 0, 0.5);
+    width: 6.25rem;
+    height: 6.25rem;
+    border-radius: 50%;
+    color: ${theme.white};
+  }
+  .swiper-button-next:after,
+  .swiper-button-prev:after {
+    font-size: 1.875rem;
+  }
+  .swiper-button-next:focus,
+  .swiper-button-prev:focus,
+  .swiper-button-next:hover,
+  .swiper-button-prev:hover {
+    border: 0.125rem solid ${theme.white};
+  }
+  .swiper-button-next {
+    right: 6.25rem;
+  }
+  .swiper-button-prev {
+    left: 6.25rem;
   }
 `;
