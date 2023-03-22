28d17a38125e95b2b5938647c8e258604925fc03
hungerbk <wjantlr@gmail.com>
2023-03-22 09:20:06 +0900

    chore: 컴포넌트 경로 수정
    - Footer, Siheky, About, Home 컴포넌트 내 import되는 컴포넌트 경로 수정(대소문자)
    
    Co-authored-by: devDobby <porphyria95@gmail.com>

--- final-cra/src/pages/Home.jsx
+++ final-cra/src/pages/Home.jsx
@@ -1,9 +1,9 @@
-import Header from 'components/Header';
-import Visual from 'components/Visual';
-import SectionOne from 'components/SectionOne';
-import FilmList from "components/filmList/FilmList";
-import Footer from 'components/footer/Footer';
-import Siheky from "components/siheky/Siheky";
+import Header from "components/Header";
+import Visual from "components/Visual";
+import SectionOne from "components/SectionOne";
+import FilmList from "components/FilmList/FilmList";
+import Footer from "components/Footer/Footer";
+import Siheky from "components/Siheky/Siheky";
 import SmoothScroll from "components/smoothScroll/SmoothScroll";
 
 export const Home = () => {
