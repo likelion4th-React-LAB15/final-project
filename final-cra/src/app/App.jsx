6ba7ef8d41ef9f5091ff4bf0f2370bd1929a1ac0
ovelute53 <ovelute53@naver.com>
2023-03-28 16:57:12 +0900

    fix: BrowserRouter -> HashRouter로 변경
    - 배포 오류로 수정

--- final-cra/src/app/App.jsx
+++ final-cra/src/app/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, BrowserRouter as Router } from 'react-router-dom';
+import { Routes, Route, HashRouter as Router } from 'react-router-dom';
 import GlobalStyle from 'components/GlobalStyle';
 import {
   Home,
